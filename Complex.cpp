2a06f188ca30e7b95d8af12353e83d1db1abe54c
speedypleath <61271015+speedypleath@users.noreply.github.com>
2020-03-15 16:41:25 +0200

    Delete Complex.cpp

--- Complex.cpp
+++
@@ -1,8 +0,0 @@
-#include "Complex.h"
-#include <bits/stdc++.h>
-
-Complex::Complex(int r=0, int i=0): r(r),i(i){}
- int Complex::get_r()const{return r;}
- int Complex::get_i()const{return i;}
- void Complex::afisare()const{std::cout<<r<<"+"<<i<<"*i";}
- std::string Complex::afisare2()const{std::string s; s+=std::to_string(r)+"+"+std::to_string(i)+"*i"; return s;}
